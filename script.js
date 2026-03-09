5a4be707d271619828e67957724c6073737b31fe
Eber <www.eberacunaquispe@gmail.com>
2026-03-09 01:17:55 -0500

    Se añadio variables para obtener valores de lo que se manda en el formulario asimosmo se utilizo el metodo preventdefault para evitarq ue el navegador recargue la pagina y se puedan obtener los datos

--- script.js
+++ script.js
@@ -15,10 +15,23 @@ function addBookToLibrary(nombre,autor,pagina,estado){
     myLibrary.push(libro)
 }
 
+function iterateItems(arr){
+    arr.forEach(i => {
+        console.log(i.title + i.author + i.pages + i.read)
+    });
+}
+
 const abrir_modal = document.getElementById("abrir_modal")
 const modal = document.getElementById("modal")
 const cerrar_modal = document.getElementById("cerrar_modal")
 const identificador = document.getElementById("identificador")
+const formulario = document.querySelector(".formulario")
+const nombre = ""
+const autor = ""
+const paginas = ""
+const leido = ""
+const noleido = ""
+const estado = ""
 
 abrir_modal.addEventListener("click",()=>{
     modal.showModal();
@@ -29,4 +42,27 @@ cerrar_modal.addEventListener("click",()=>{
     modal.close();
 })
 
+formulario.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    nombre = document.getElementById("nombre")
+    autor = document.getElementById("autor")
+    paginas = document.getElementById("paginas")
+    leido = document.getElementById("leido")
+    noleido = document.getElementById("noleido")
+
+    if(leido.checked){
+        estado = "Read"
+        addBookToLibrary(nombre,autor,paginas,estado)
+    }
+
+    if(noleido.checked){
+        estado = "Unread"
+        addBookToLibrary(nombre,autor,paginas,estado)
+    }
+
+    if(!noleido.checked && !leido.checked){
+        alert("Debe marcar el estado del libro")
+    }
+
+})
 
