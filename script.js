81be4277de3298786990f1ae2b6c5cde94da01f3
Eber <www.eberacunaquispe@gmail.com>
2026-03-09 15:23:51 -0500

    Se creo la funcion addcart que crea cartas tomando la ultuima posicion de los elementos almacenadoe en el array aun hay errores con los id generados

--- script.js
+++ script.js
@@ -15,13 +15,30 @@ function addBookToLibrary(nombre,autor,pagina,estado){
     myLibrary.push(libro)
 }
 
-function iterateItems(arr){
-    arr.forEach(i => {
-        campo.textContent(i.title)
-        campo.textContent(i.author)
-        campo.textContent(i.pages)
-        campo.textContent(i.read)
-    });
+function addCart(arr){
+    const cartas = document.createElement("div")
+    cartas.classList.add("editarlibro")
+    const numero_libro = document.createElement("p")
+    numero_libro.classList.add("editarid")
+    const cartanombre = document.createElement("p")
+    cartanombre.classList.add("editarnombre")
+    const cartaautor = document.createElement("p")
+    cartaautor.classList.add("editarautor")
+    const cartapagina = document.createElement("p")
+    cartapagina.classList.add("editarpagina")
+    const cartaestado = document.createElement("p")
+    cartaestado.classList.add("editarestado")
+    numero_libro.textContent = id       
+    cartanombre.textContent = arr.at(-1).title
+    cartaautor.textContent = arr.at(-1).author
+    cartapagina.textContent = arr.at(-1).pages
+    cartaestado.textContent = arr.at(-1).read
+    campo.appendChild(cartas)
+    cartas.appendChild(numero_libro)
+    cartas.appendChild(cartanombre)
+    cartas.appendChild(cartaautor)
+    cartas.appendChild(cartapagina)
+    cartas.appendChild(cartaestado)
 }
 
 const abrir_modal = document.getElementById("abrir_modal")
@@ -31,6 +48,7 @@ const identificador = document.getElementById("identificador")
 const formulario = document.querySelector(".formulario")
 const llenar = document.querySelector(".llenar")
 const campo = document.querySelector(".campo")
+let id
 let nombre 
 let autor 
 let paginas 
@@ -40,7 +58,7 @@ let estado
 
 abrir_modal.addEventListener("click",()=>{
     modal.showModal();
-    const id = identificador.textContent = `ID: ${crypto.randomUUID()}` 
+    id = identificador.textContent = `ID: ${crypto.randomUUID()}`
 })
 
 cerrar_modal.addEventListener("click",()=>{
@@ -68,10 +86,12 @@ formulario.addEventListener("submit",(e)=>{
         estado = leido.value
         console.log(estado)
         addBookToLibrary(nombre,autor,paginas,estado)
+        addCart(myLibrary)
     }else if(noleido.checked){
         estado = noleido.value
         console.log(estado)
         addBookToLibrary(nombre,autor,paginas,estado)
+        addCart(myLibrary)
     }
     llenar.reset();
 })
