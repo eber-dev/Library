7d57cfa9caa8a6bdf5c6c8ba66bc4a9ebba3de0a
Eber <www.eberacunaquispe@gmail.com>
2026-03-09 00:29:00 -0500

    Se genera los id por cada vez que se abre el formulario de registro de libros

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function Book(title, author , pages, read){
     if(!new.target){
         throw Error("Usted debe utilizar el operador new para llamar al constructor")
     }
-    id: crypto.randomUUID()
     this.title = title
     this.author = author
     this.pages = pages
@@ -19,11 +18,15 @@ function addBookToLibrary(nombre,autor,pagina,estado){
 const abrir_modal = document.getElementById("abrir_modal")
 const modal = document.getElementById("modal")
 const cerrar_modal = document.getElementById("cerrar_modal")
+const identificador = document.getElementById("identificador")
 
 abrir_modal.addEventListener("click",()=>{
     modal.showModal();
+    const id = identificador.textContent = `ID: ${crypto.randomUUID()}` 
 })
 
 cerrar_modal.addEventListener("click",()=>{
     modal.close();
 })
+
+
